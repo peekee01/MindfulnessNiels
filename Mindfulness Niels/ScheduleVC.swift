494fed895a5290a260a86f13e96f28c2a9f8aedd
Pieter Kuijsten <peekee@gmail.com>
2017-01-25 00:11:36 +0100

    werkend menu

---
+++ Mindfulness Niels/ScheduleVC.swift
@@ -0,0 +1,52 @@
+//
+//  ScheduleVC.swift
+//  Mindfulness Niels
+//
+//  Created by Pieter Kuijsten on 24/01/2017.
+//  Copyright © 2017 Pieter Kuijsten. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleVC: UIViewController {
+    
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    var menuShowing = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuView.layer.opacity = 0.6
+        menuView.layer.shadowOpacity = 0.5
+        menuView.layer.shadowRadius = 6
+    }
+    
+    
+    @IBAction func openMenu(_ sender: Any) {
+        if menuShowing == false {
+            leadingConstraint.constant = 0
+            UIView.animate(withDuration: 0.5, animations: {self.view.layoutIfNeeded() })
+            
+            menuShowing = true
+        } else {
+            leadingConstraint.constant = -190
+            UIView.animate(withDuration: 0.5, animations: {self.view.layoutIfNeeded() })
+            
+            menuShowing = false
+        }
+    }
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
