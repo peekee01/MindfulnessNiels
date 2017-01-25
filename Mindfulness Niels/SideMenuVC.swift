6f7808a971b27af1f1bb3a380d27e1700f364694
Pieter Kuijsten <peekee@gmail.com>
2017-01-25 19:26:58 +0100

    Toch met ENSideMenu

---
+++ Mindfulness Niels/SideMenuVC.swift
@@ -0,0 +1,27 @@
+//
+//  SideMenuVC.swift
+//  Mindfulness Niels
+//
+//  Created by Pieter Kuijsten on 25/01/2017.
+//  Copyright © 2017 Pieter Kuijsten. All rights reserved.
+//
+
+import UIKit
+import ENSwiftSideMenu
+
+class SideMenuVC: ENSideMenuNavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let menu = storyboard.instantiateViewController(withIdentifier: "MenuTableVC") as! MenuTableVC
+
+        sideMenu = ENSideMenu(sourceView: self.view, menuViewController: menu, menuPosition: ENSideMenuPosition.left)
+        sideMenu?.menuWidth = 180
+        view.bringSubview(toFront: navigationBar)
+    }
+
+    
+}
