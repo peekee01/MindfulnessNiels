1545dea6d64a40d3b62cae66e15337de6ab432fc
Pieter Kuijsten <peekee@gmail.com>
2017-01-30 22:42:51 +0100

    met slider en timer

--- Mindfulness Niels/AudioPlayerVC.swift
+++ Mindfulness Niels/AudioPlayerVC.swift
@@ -12,13 +12,22 @@ import AVFoundation
 class AudioPlayerVC: UIViewController {
     
     @IBOutlet weak var pausePlay: UIButton!
+    @IBOutlet weak var audioSlider: UISlider!
+    @IBOutlet weak var timeElapsed: UILabel!
+    @IBOutlet weak var timeRemaining: UILabel!
     
-    var AudioURL = URL(fileURLWithPath: Bundle.main.path(forResource: "10. Mindfulness of the Breath", ofType: "mp3")!)
+    var AudioURL = URL(fileURLWithPath: Bundle.main.path(forResource: "22 De soldaat", ofType: "mp3")!)
     var AudioPlayer: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         loadAudioPlayer()
+        audioSlider.maximumValue = Float(AudioPlayer!.duration)
+        print(TimeInterval(AudioPlayer!.duration))
+        
+        _ = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(AudioPlayerVC.updateAudioSlider), userInfo: nil, repeats: true)
+        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(AudioPlayerVC.updateTimeLabels), userInfo: nil, repeats: true)
     }
     
     override func didReceiveMemoryWarning() {
@@ -36,8 +45,7 @@ class AudioPlayerVC: UIViewController {
             print("error: \(error.localizedDescription)")
         }
     }
-  
-
+    
     @IBAction func pausePlayBtn(_ sender: UIButton) {
         if AudioPlayer!.isPlaying == true {
             AudioPlayer!.stop()
@@ -48,6 +56,38 @@ class AudioPlayerVC: UIViewController {
         }
     }
     
-
+    @IBAction func changeAudioTime(_ sender: UISlider) {
+        AudioPlayer!.stop()
+        AudioPlayer!.currentTime = TimeInterval(audioSlider.value)
+        AudioPlayer!.play()
+        pausePlay.setImage(UIImage(named: "Audio_pause.png"), for: UIControlState.normal)
+    }
+    
+    func updateAudioSlider() {
+        audioSlider.value = Float(AudioPlayer!.currentTime)
+    }
+    
+    func updateTimeLabels() {
+        let elapsedSeconds = Int(AudioPlayer!.currentTime) % 60
+        let elapsedMinutes = (Int(AudioPlayer!.currentTime) / 60) % 60
+        let remainingSeconds = Int(AudioPlayer!.duration - AudioPlayer!.currentTime) % 60
+        let remainingMinutes = (Int(AudioPlayer!.duration - AudioPlayer!.currentTime) / 60) % 60
+        
+        timeElapsed.text = String(format: "%0.2d:%0.2d",elapsedMinutes,elapsedSeconds)
+        timeRemaining.text = String(format: "%0.2d:%0.2d",remainingMinutes,remainingSeconds)
+    }
+    
+    //    func updateRemainingTime() {
+    //        var seconds = 0
+    //        var minutes = 0
+    //        let time = AudioPlayer!.duration - AudioPlayer!.currentTime
+    //        seconds = Int(time) % 60
+    //        minutes = (Int(time) / 60) % 60
+    //
+    //
+    //        timeRemaining.text = String(format: "%0.2d:%0.2d",minutes,seconds)
+    //    }
+    
+    
     
 }
